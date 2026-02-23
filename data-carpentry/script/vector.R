3afda8437564c24b83350a5c3a6d196940cd7f62
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 15:41:13 +0100

    gitautosync update 46

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -93,5 +93,6 @@ unique(lines_Delft$highway)
 road_types <- c("motorway", "primary", "secondary", "cycleway")
 
 lines_Delft_selection <- lines_Delft |> 
-  filder(highway %in% road_types)
+  filder(highway %in% road_types) |> 
+  
 
