a3ebd1876ce044b73573121bfababd94741e4c7a
javisanmillan <javitenas@gmail.com>
2026-02-23 12:36:25 +0100

    gitautosync update 52

--- data-carpentry/script/intro-to-r.R
+++ data-carpentry/script/intro-to-r.R
@@ -186,11 +186,13 @@ gapminder |>
   geom_col() +
   coord_flip()
 
-
+# Ordering countries
 gapminder |> 
   filter(year == 2007 & continent == "Americas") |> 
   mutate(country = fct_reorder(country, gdpPercap)) |> 
   ggplot(aes(x = country, y = gdpPercap)) +
   geom_col() +
   coord_flip()
+
+
 
