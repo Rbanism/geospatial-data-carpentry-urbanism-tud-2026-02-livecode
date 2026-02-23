348cab28ee47a1d384eccd025968616b6199a92e
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 14:21:44 +0100

    gitautosync update 52

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -26,7 +26,7 @@ boundary_Delft
 ggplot(data=boundary_Delft) +
   geom_sf(size=3, color="black", fill="cyan1") +
   labs(title="Delft Administrative Boundary") +
-  coord_sf(ddatum=st)
+  coord_sf(ddatum=stcrs(28992))
   
   
   
