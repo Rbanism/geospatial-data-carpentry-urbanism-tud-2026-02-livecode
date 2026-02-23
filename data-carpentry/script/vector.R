01c0cd12fc95ecbd0518f1b3b1ebf680b7321597
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 16:42:08 +0100

    gitautosync update 186

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -169,5 +169,5 @@ ggplot() +
     values = leisure_colors,
     name = "Leisure Location"
   ) +
-  labs(title = "Mobility network adn leisureof Delft") +
+  labs(title = "Mobility network adn leisure in Delft") +
   coord_sf(datum = st_crs(28992))
