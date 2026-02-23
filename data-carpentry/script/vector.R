9ffecbb7396458dc0b12401fe545b28d8102b146
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 15:49:13 +0100

    gitautosync update 66

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -100,4 +100,4 @@ road_colors <- c("blue", "green", "navy", "purple")
 
 ggplot(data = lines_Delft_selection) + 
   geom_sf(aes(color = highway)) +
-  scale_color_manual()
+  scale_color_manual(values = road_colors)
