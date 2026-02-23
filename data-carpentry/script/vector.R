b37f03c9450e23e4e730f6a919a65fa8131e806c
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 15:07:41 +0100

    gitautosync update 11

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -72,7 +72,7 @@ slow_Delft <- lines_Delft |>
 nrow(slow_Delft)
 unique(slow_Delft$highway)
 
-cycleway_Delft <- cycleway_Delft |> 
+cycleway_Delft |> 
   mutate(length = st_length(geometry))
 
 cycleway_Delft |> 
