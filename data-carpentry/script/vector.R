dec5a13056fac9a4f005222862cc437245d031d4
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 14:30:27 +0100

    gitautosync update 56

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -28,6 +28,7 @@ ggplot(data=boundary_Delft) +
   labs(title="Delft Administrative Boundary") +
   coord_sf(datum=st_crs(28992)) # display the axis in metres
 
-  
+lines_Delft <- st_read(here("data", "delft-streets.shp"))
+point_Delft <- st_read(here("data", "delft-leisure.shp"))
   
   
