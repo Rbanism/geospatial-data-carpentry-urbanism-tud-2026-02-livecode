11a46089c4e25d7c3b6a4cb2c6a836ab2e036bfe
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-23 15:56:15 +0100

    gitautosync update 84

--- data-carpentry/script/vector.R
+++ data-carpentry/script/vector.R
@@ -101,7 +101,7 @@ road_colors <- c("blue", "green", "navy", "purple")
 
 ggplot(data = lines_Delft_selection) + 
   geom_sf(aes(color = highway)) +
-  scale_color_manual(values = road_colors) +
+  # scale_color_manual(values = road_colors) +
   labs(
     color = "Road Type",
     title = "Mobilitt network in Delft",
