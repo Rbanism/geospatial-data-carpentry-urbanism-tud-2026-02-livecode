7166b823474c775264da51396575d068aa0c90de
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-26 10:56:45 +0100

    gitautosync update 143

--- data-carpentry/script/raster.R
+++ data-carpentry/script/raster.R
@@ -73,4 +73,4 @@ ggplot() +
               aes(x = x,
                   y = y,
                   fill = fct_elevation_cb)) +
-  scale_fill_manual(values = )
+  scale_fill_manual(values = terrain.colors(3))
