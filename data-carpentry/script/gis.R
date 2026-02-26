88722078dc632bed55acee0e9edd161cce64c620
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 14:05:50 +0100

    gitautosync update 98

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -53,5 +53,5 @@ buildings$build_date <- if_else(
 ggplot(data = buildings) +
   geom_sf(aes(fill = build_date))+ #group1&2
  # geom_sf(aes(colour = build_date))+ #group3&4
-scale_
+  scale_fill_viridis_c()
 
