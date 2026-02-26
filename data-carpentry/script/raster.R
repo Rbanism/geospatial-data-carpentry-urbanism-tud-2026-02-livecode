f19d2a6b14fc4b082623b4a379beed82fb8c3be5
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-26 10:40:36 +0100

    gitautosync update 92

--- data-carpentry/script/raster.R
+++ data-carpentry/script/raster.R
@@ -38,5 +38,5 @@ nlyr(DSM_TUD)
 
 # Plot raster data
 
-DSM_TUD_df |>
-  mutate(fct_elevation = cut(`tud-dsm-5m`, breaks = ))
+DSM_DSM_TUD_df |>
+  mutate(fct_elevation = cut(`tud-dsm-5m`, breaks = 3))
