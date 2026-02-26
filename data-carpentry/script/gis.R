6f7bb5ac8802962160751805c3da54abb7c73829
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 16:02:31 +0100

    gitautosync update 236

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -179,5 +179,6 @@ ggplot(data = buffer_old_buildings) +
   coord_sf(datum = st_crs(28992))
 
 # Union
-single_old_buffer <- st_union(buffer_old_buildings)
+single_old_buffer <- st_union(buffer_old_buildings) |>
+  st_cast()
 
