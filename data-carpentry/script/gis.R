3d24da6a1387c60eacb3a2d53bae26dcc2d26b2f
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 13:59:20 +0100

    gitautosync update 76

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -39,6 +39,6 @@ buildings <- x$osm_polygons |>
   st_transform(crs = 28992)
 
 buildings$build_date <- if_else(buildings$start_date < 1900,# the condition
-                                1900,
-                                buildings$start_date)
+                                1900, #if yes
+                                buildings$start_date) #if no
 
