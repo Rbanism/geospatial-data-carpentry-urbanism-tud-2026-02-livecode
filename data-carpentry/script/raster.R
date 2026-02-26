66831e7960534c42dd2ae71c462d5fd675dfb72e
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-26 10:50:33 +0100

    gitautosync update 121

--- data-carpentry/script/raster.R
+++ data-carpentry/script/raster.R
@@ -54,5 +54,4 @@ levels(DSM_TUD_df$fct_elevation_cb)
 
 # plot the new breaks
 ggplot() +
-  geom_bar(data = DSM_TUD_df, aes(fct_elevation_cb
-                                  ))
+  geom_bar(data = DSM_TUD_df, aes(fct_elevation_cb))
