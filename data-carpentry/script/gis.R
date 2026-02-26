7d389dc52c46ea5bdb8067195fa3d3bea1710b35
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 13:32:22 +0100

    gitautosync update 37

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -14,7 +14,7 @@ bb
 
 # extracting features with opq()
 
-assign("has_internet_via_proxy", TRUE, environment)
+assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
 
 x <- opq(bbox = bb) |>
   add_osm_feature(key = "building") |>
