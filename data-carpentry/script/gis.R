b53dedc37727d9a86bef2a0c2f4ceb8e7ab762e3
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 13:47:48 +0100

    gitautosync update 54

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -28,6 +28,7 @@ x <-opq(bbox = bb, timeout = 10000) |>
   add_osm_feature(key = "building") |>
   osmdata_sf()
 
+#explore the result
 str(x$osm_polygons)
 head(x$osm_polygons)
 summary(x$osm_polygons)
