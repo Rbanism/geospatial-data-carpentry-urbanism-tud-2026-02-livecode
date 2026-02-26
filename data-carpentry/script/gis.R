385e2efda95d88171b76696adc5439b4d2a1840f
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 15:05:27 +0100

    gitautosync update 150

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -99,7 +99,7 @@ buildings2 <- buildings |>
 leaflet(buildings2) |>
   addProviderTiles(providers$CartoDB.Positron) |>
   addPolygons(
-  fillColor = ~ colorQuantile("YlGnBu")
+  fillColor = ~ colorQuantile("YlGnBu", -build_date)(-)
   )
 
 
