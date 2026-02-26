cef24d8826940b2dc4ab21443ba7a1e1b41fbe13
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-26 11:21:55 +0100

    gitautosync update 198

--- data-carpentry/script/raster.R
+++ data-carpentry/script/raster.R
@@ -99,9 +99,6 @@ ggplot() +
 DTM_TUD <- rast("data/tud-dtm-5m.tif")
 DTM_TUD_hill <- rast("data/tud-dtm-5m-hill-WGS84.tif")
 
-crs(DTM_TUD)
-crs(DTM_TUD_hill)
-
 # Create data frames for plotting
 DTM_TUD_df <- as.data.frame(DTM_TUD, xy = TRUE)
 DTM_TUD_hill_df <- as.data.frame(DTM_TUD_hill, xy = TRUE)
