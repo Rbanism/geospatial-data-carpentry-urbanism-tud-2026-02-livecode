46671ef42c75b7046de5219b860f187e1a4f3a24
Claudiu Forgaci <claudiuforgaci@gmail.com>
2026-02-26 09:57:06 +0100

    gitautosync update 39

--- data-carpentry/script/raster.R
+++ data-carpentry/script/raster.R
@@ -16,7 +16,7 @@ DSM_TUD <- rast("data/tud-dsm-5m.tif")
 str(DSM_TUD)
 View(DSM_TUD)
 summary(DSM_TUD)  # but this uses a sample and the min/max might not be accurate
-summary(values(DSM_TUD)). 
+summary(values(DSM_TUD))  
 
 
 
