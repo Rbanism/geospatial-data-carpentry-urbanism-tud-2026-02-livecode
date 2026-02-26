7e01efbde855f42fa23996fdb150648e4e531352
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 16:07:34 +0100

    gitautosync update 248

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -185,5 +185,5 @@ single_old_buffer <- st_union(buffer_old_buildings) |>
 
 single_old_buffer <- single_old_buffer |>
   mutate("ID" = as.factor(seq_len(nrow(single_old_buffer)))) |>
-
+ st_transform(crs = 28992)
 
