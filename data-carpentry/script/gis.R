d8ca4bf4994afd1e0900669dca5052ac56a0a8f5
Clementine Cottineau <c.cottineau@tudelft.nl>
2026-02-26 16:15:41 +0100

    gitautosync update 273

--- data-carpentry/script/gis.R
+++ data-carpentry/script/gis.R
@@ -197,4 +197,5 @@ centroids_old <- st_centroid(old_buildings) |>
 
 ggplot() +
   geom_sf(data = single_old_buffer, aes(fill = ID)) +
-  geom_sf(data = )
+  geom_sf(data = centroids_old) +
+  coord_sf(datum = st_crs(28992))
